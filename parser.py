8fe4be529c058fca5e4a0b4b1f445577a2786021
TuLe <tale4@usfca.edu>
2023-03-31 21:52:19 -0700

    Dictionary

--- parser.py
+++ parser.py
@@ -16,9 +16,9 @@ with open("HackCopy.csv", 'r') as file:
         """
         Format of the dictionary:
         - Key = Course number (Ex: AEM120, CS272)
-        - Value = A list where:
+        - Value = A set (to remove duplicate times) where:
             + First index = Course title
-            + After: A string that looks like "d, time", where d is the day of the week (M,T,W,R,F)
+            + After: A string that looks like "d, time", where d is the day of the week (M,T,W,R,F,S)
             and time is the time separated by a ',' so a split(',') function should work 
             -> And the times have this format: from [am/pm]- to [am/pm] -> so another split('-') maybe?
         """
@@ -28,12 +28,11 @@ with open("HackCopy.csv", 'r') as file:
             is to remove those course entirely from the dictionary. You can comment them out to see the problem.
             The problematic course is UPA667
             """
-            if day not in "MTWRF":
+            if len(day) > 4 or day not in "MTWRFS":
                 continue
-
             if cur_course_number not in course_dict:
-                course_dict[cur_course_number] = [row[6]]
-            course_dict[cur_course_number] += [day + ", " + row[8]]
+                course_dict[cur_course_number] = {row[6]}
+            course_dict[cur_course_number].add(day + ", " + row[8])
 
     # To get a specific course info, do:
     # print(course_dict['course_number'])
