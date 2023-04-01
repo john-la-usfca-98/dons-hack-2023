5343408c061cbd07988418ef40b185aeda711141
aajoseph2 <89800875+aajoseph2@users.noreply.github.com>
2023-04-01 01:47:03 -0700

    need to add comments, but parser fully works. Just need to formulate schedules now

--- parser.py
+++ parser.py
@@ -1,11 +1,15 @@
 import csv
 
 course_dict = {}
-with open("HackCopy.csv", 'r') as file:
+with open("/Users/amin/dons-hack-2023/HackCopy.csv", 'r') as file:
     csvreader = csv.reader(file)
     next(csvreader)
     for row in csvreader:
-        cur_course_number = row[1] + row[2]    # Getting the course name
+        valueList = []
+        #This is meant to be the list that represents the name of the class[0]
+        #The the days of the week that the class is offered[1]
+        #and the the time[2]
+        cur_course_number = row[1] + row[2] +  "-" + row[3]    # Getting the course name
 
         # To deal with empty rows
         if cur_course_number == "":
@@ -13,36 +17,61 @@ with open("HackCopy.csv", 'r') as file:
         else:
             course_number = cur_course_number
 
-        """
-        Format of the dictionary:
-        - Key = Course number (Ex: AEM120, CS272)
-        - Value = A list where:
-            + First index = Course title
-            + After: A string that looks like "d, time", where d is the day of the week (M,T,W,R,F,S)
-            and time is the time separated by a ',' so a split(',') function should work 
-            -> And the times have this format: from [am/pm]- to [am/pm] -> so another split('-') maybe?
-        """
-        for day in row[7]:
-            """
-            Some course titles have ',' in them so the parsing is kind of messed up. The next 2 lines of code
-            is to remove those course entirely from the dictionary. You can comment them out to see the problem.
-            The problematic course is UPA667
-            """
-            if len(row[7]) > 4 or day not in "MTWRFS":
-                continue
-            if cur_course_number not in course_dict:
-                course_dict[cur_course_number] = [row[6]]
-            time = day + ", " + row[8]
-            # To remove duplicate times
-            if time not in course_dict[cur_course_number]:
-                course_dict[cur_course_number] += [time]
+        #I made the value of the key to be a list where each index represents a different 
+        #info
+
+        valueList.append("Sec: " + row[3])
+        valueList.append(row[6]) 
+        valueList.append(row[7])
+        valueList.append(row[8])
+
+
+        course_dict[cur_course_number] = valueList
+
+
 
     # To get a specific course info, do:
-    print(course_dict['AEM120'][0])
-    print(course_dict)  # Uncomment this to see how the dictionary looks.
+    #print(course_dict['AEM120'][0])
+    sorted_course_dict = dict(sorted(course_dict.items()))
+
+    #print(sorted_course_dict)  # Uncomment this to see how the dictionary looks.
 
 """
 Feel free to ask me anything
 """
-#def get_schedule(class_list): that returns a list of all possible schedules for a given list of class
+#that returns a list of all possible schedules for a given list of class 
+
+classId = "AEM124,ARCH350,BIOL105,BUS100"
+
+    
+#classId = input("Please enter your class Id: ")
+
+def get_schedule(classId): 
+    updatedDict = {}
+    classIdList = classId.split(',')
+    
+    for course in classIdList:
+        prefix = course.split('-')[0].lower()  # extract the course prefix and convert to lowercase
+        if prefix not in [key.split('-')[0].lower() for key in course_dict.keys()]:
+            print("No courses found for prefix:", prefix)
+        else:
+            for key in course_dict.keys():
+                if key.lower().startswith(prefix):  # convert key to lowercase before checking
+                    print(course_dict[key])
+                    updatedDict[key] = course_dict[key]
+
+    return updatedDict
+
+
+selectedClass = get_schedule(classId)
+
+#print(get_schedule(classId))
+
+def checkConflict(selectedClass):
+
+    return 0;
+
+
+
+
 
