f91051b49306efbf010fdbc83258614914af993c
ajcatoiu <ajcatoiu@dons.usfca.edu>
2023-04-01 16:04:10 -0700

    background

--- Schedule/back.py
+++ Schedule/back.py
@@ -1,4 +1,3 @@
-#import scheduleParser
 from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
---
+++ Schedule/prof.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+def main():
+    getProf(getDriver())
+
+def getDriver():
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_experimental_option("detach", True)
+    chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get("https://www.ratemyprofessors.com/search/teachers?query=*&sid=1600")
+    return driver
+
+
+def getProf(pDriver):
+    rating = WebDriverW
