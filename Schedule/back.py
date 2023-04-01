81b5a27d9c19c8e77a98c3f5865034443987369b
TuLe <tale4@usfca.edu>
2023-04-01 16:45:20 -0700

    Radio buttons

--- Schedule/back.py
+++ Schedule/back.py
@@ -8,10 +8,11 @@ def fSchedule():
     if request.method == 'POST':
         inputs = request.form['classIds']
         schedules = scheduleParser.get_schedule(inputs)
-        ranked = scheduleParser.rank_schedules(schedules, "bunch", "late")
+        ranked = scheduleParser.rank_schedules(schedules, request.form['range'], request.form['time'])
         ranked = scheduleParser.print_rank_schedules(ranked)
         print(ranked)
-        return render_template('schedule.html', schedules=ranked)
+        options = "Courses: " + request.form['classIds'] + ", Spread: " + request.form['range'] + ", Time: " + request.form['time']
+        return render_template('schedule.html', schedules=ranked, options=options)
     return render_template('schedule.html', schedules=schedules)
 
 if __name__ == '__main__':
