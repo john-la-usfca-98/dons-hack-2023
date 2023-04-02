bba8b6e8b5d243d6e75506b9139d2e3ffcdc264c
ajcatoiu <ajcatoiu@dons.usfca.edu>
2023-04-01 17:52:07 -0700

    scraping script

--- Schedule/prof.py
+++ Schedule/prof.py
@@ -15,6 +15,35 @@ def getDriver():
     return driver
 
 def getProf(pDriver):
-    rating = WebDriverWait(pDriver, 5).until(
-        EC.presence_of_element_located(By.XPATH, "")
-    )
+    
+    button = WebDriverWait(pDriver, 2).until(
+            EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div/div/div[4]/div[1]/div[1]/div[4]/button"))
+        )
+    
+    button.click()
+
+
+    button = pDriver.find_element_by_xpath("/html/body/div[2]/div/div/div[4]/div[1]/div[1]/div[4]/button")
+    
+    i = 1
+    while (i != 20):
+
+   
+
+        ratingPath = "/html/body/div[2]/div/div/div[4]/div[1]/div[1]/div[3]/a[{}]/div/div[1]/div/div[2]".format(i)
+        namePath = "/html/body/div[2]/div/div/div[4]/div[1]/div[1]/div[3]/a[{}]/div/div[2]/div[1]".format(i)
+        rating = WebDriverWait(pDriver, 2).until(
+            EC.presence_of_element_located((By.XPATH, ratingPath))
+        )
+        name = WebDriverWait(pDriver, 2).until(
+            EC.presence_of_element_located((By.XPATH, namePath))
+        )
+        ratingVal = rating.text
+        nameVal = name.text
+        print(ratingVal, nameVal, sep="; ")
+        i += 1
+
+
+    
+
+main()
