813362b20c872a834943ffddb5aa4c95e072b0da
TuLe <tale4@usfca.edu>
2023-04-01 21:33:51 -0700

    Schedule event calendar

--- Schedule/back.py
+++ Schedule/back.py
@@ -14,15 +14,31 @@ def fSchedule():
         ranked = scheduleParser.rank_schedules(schedules, request.form['range'], request.form['time'])
         ranked = scheduleParser.print_rank_schedules(ranked)
         len_schedules = request.form['show_list']
+
         if len_schedules == "show_all":
             len_schedules = len(schedules)
         else:
             len_schedules = min(len(schedules), 5)
-        #print(ranked)
+
+        schedule_list = []
+        for i in range(len_schedules):
+            possible_schedule_dict = {}
+            for schedule in ranked[i]:
+                info = schedule.split(',')
+                course_num = info[0].split(" ")[2][:-1]
+                days = info[1].replace(" ", "")
+                time = info[2][:18]
+                for day in days:
+                    if day not in possible_schedule_dict:
+                        possible_schedule_dict[day] = []
+                    possible_schedule_dict[day].append(course_num)
+            schedule_list.append(possible_schedule_dict)
+        print(schedule_list)
+
         options = "Courses: " + request.form['classIds'] + ", Spread: " + request.form['range'] + ", Time: " + \
                   request.form['time']
         return render_template('schedule.html', schedules=ranked, options=options, len_schedules=len_schedules,
-                               not_class=not_class, is_class=is_class)
+                               not_class=not_class, is_class=is_class, schedule_list=schedule_list)
     return render_template('schedule.html', schedules=schedules)
 
 if __name__ == '__main__':
--- Schedule/scheduleParser.py
+++ Schedule/scheduleParser.py
@@ -6,13 +6,13 @@ from datetime import datetime
 import itertools
 import sys
 
+
 def parse_time(t):
     try:
         return datetime.strptime(t, '%I:%M %p').time()
     except ValueError:
         return datetime.strptime(t, '%I:%M%p').time()
 
-course_dict = {}
 
 # Change this part of your code
 url = "https://drive.google.com/file/d/1VBgk_-EiNG3idVckxQpzzKedVYlqQGVH/view?usp=share_link"
@@ -30,32 +30,36 @@ csv_data = StringIO(response.text)
 csvreader = csv.reader(csv_data)
 next(csvreader)
 
-for row in csvreader:
-    valueList = []
+course_dict = {}
+with open("HackCopy.csv", 'r') as file:
+    csvreader = csv.reader(file)
+    next(csvreader)
+    for row in csvreader:
+        valueList = []
 
-    cur_course_number = row[1] + row[2] +  "-" + row[3]
+        cur_course_number = row[1] + row[2] + "-" + row[3]
 
-    if cur_course_number == "":
-        cur_course_number = course_number
-    else:
-        course_number = cur_course_number
-    valueList.append("CRN: " + row[0])
-    valueList.append("Sec: " + row[3])
-    valueList.append(row[7])
-    if '-' in row[8]:
-        start_time, end_time = row[8].split('-')
-        valueList.append(start_time.strip())
-        valueList.append(end_time.strip())
-    else:
-        valueList.append(row[8].strip())
-        valueList.append('')  # Add an empty string for end_time if it's not available
+        if cur_course_number == "":
+            cur_course_number = course_number
+        else:
+            course_number = cur_course_number
+        valueList.append("CRN: " + row[0])
+        valueList.append("Sec: " + row[3])
+        valueList.append(row[7])
+        if '-' in row[8]:
+            start_time, end_time = row[8].split('-')
+            valueList.append(start_time.strip())
+            valueList.append(end_time.strip())
+        else:
+            valueList.append(row[8].strip())
+            valueList.append('')  # Add an empty string for end_time if it's not available
 
-    valueList.append(row[18])
-    #Instructor name^
-    course_dict[cur_course_number] = valueList
+        valueList.append(row[18])
+        # Instructor name^
+        course_dict[cur_course_number] = valueList
 
-argu = sys.argv[1]
-classId = sys.argv[1]
+#argu = sys.argv[1]
+#classId = sys.argv[1]
 
 
 def check_conflict(class1, class2):
@@ -71,15 +75,22 @@ def check_conflict(class1, class2):
         return True
     return False
 
-def get_schedule(classId): 
+
+def get_schedule(classId):
+    classId = classId.replace(" ", "")
     class_sections = {}
     classIdList = classId.split(',')
-    
+    res = []
+    not_class = []
+    is_class = []
+
     for course in classIdList:
         prefix = course.split('-')[0].lower()
         if prefix not in [key.split('-')[0].lower() for key in course_dict.keys()]:
             print("No courses found for prefix:", prefix)
+            not_class += [prefix]
         else:
+            is_class += [prefix.upper()]
             class_sections[prefix] = []
             for key in course_dict.keys():
                 if key.lower().startswith(prefix):
@@ -100,11 +111,10 @@ def get_schedule(classId):
 
         if not conflict:
             possible_schedules.append(section_combinations)
-
-    return possible_schedules
-
-#def profRating(schedule)
- #   profName = schedule
+    res.append(possible_schedules)
+    res.append(not_class)
+    res.append(is_class)
+    return res
 
 
 # Add the following functions to compute the ranking score based on user preferences
@@ -114,6 +124,7 @@ def compute_spread_score(schedule):
         days.update(set(course_dict[course][2]))
     return len(days)
 
+
 def compute_time_preference_score(schedule, time_preference):
     total_score = 0
     for course in schedule:
@@ -122,6 +133,7 @@ def compute_time_preference_score(schedule, time_preference):
         elif time_preference == "late":
             total_score += parse_time(course_dict[course][3]).hour
     return total_score
+
 
 def rank_schedules(schedules, spread_preference, time_preference):
     ranked_schedules = sorted(
@@ -134,14 +146,16 @@ def rank_schedules(schedules, spread_preference, time_preference):
     )
     return ranked_schedules
 
-# Get the schedules
-schedules = get_schedule(classId)
 
+# Get the schedules
+# schedules = get_schedule(classId)
+"""
 if len(schedules) == 0:
     print("There are no possible schedules.")
     sys.exit()
-
+"""
 # After getting the schedules, ask for user preferences
+"""
 spread_preference = input("Do you want your courses spread out through the week or bunched up together? (spread/bunch): ").lower()
 while spread_preference not in ("spread", "bunch"):
     spread_preference = input("Invalid input. Please enter 'spread' or 'bunch': ").lower()
@@ -152,23 +166,27 @@ while time_preference not in ("early", "late"):
 
 # Rank the schedules based on user preferences
 ranked_schedules = rank_schedules(schedules, spread_preference, time_preference)
+"""
+
 
 # Display the top 5 ranked schedules
-print("\nTop 5 schedules based on your preferences:")
-for i, schedule in enumerate(ranked_schedules[:5]):
-    print(f"Rank {i+1}:")
-    for course in schedule:
-        course_info = course_dict[course]
-        print(f"{course_info[0]} {course}: {course_info[1]}, {course_info[2]}, {course_info[3]}-{course_info[4]} {course_info[5]}")
-    print("\n")
+def print_rank_schedules(ranked_schedules):
+    res = []
+    for i, schedule in enumerate(ranked_schedules):
+        temp = []
+        for course in schedule:
+            course_info = course_dict[course]
+            temp += [course_info[0] + " " + course + ": " + course_info[1] + ", " + course_info[2] + ", " + course_info[
+                3] + "-" + course_info[4] + " " + course_info[5]]
+        res.append(temp)
+    return res
 
+
+"""
 # Give the user the choice to display all schedule combinations
 show_all = input("Do you want to see all the schedule combinations? (yes/no): ").lower()
 while show_all not in ("yes", "no"):
     show_all = input("Invalid input. Please enter 'yes' or 'no': ").lower()
-
-
-
 
 if show_all == "yes":
     print("\nAll schedule combinations:")
@@ -178,9 +196,4 @@ if show_all == "yes":
             course_info = course_dict[course]
             print(f"{course_info[0]} {course}: {course_info[1]}, {course_info[2]}, {course_info[3]}-{course_info[4]} {course_info[5]}")
         print("\n")
-
-
-
-
-
-
+"""
